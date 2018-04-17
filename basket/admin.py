c5d1e64bc3a69cb6ea01baee5536b45fb7f9ec96
Rodrigo <asd@asd.com>
2018-04-17 20:26:53 -0300

    Modelamiento listo

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from basket.models import *
 
 # Register your models here.
+# admin.site.register(Team)
+# admin.site.register(Player)
+# admin.site.register(Couch)
+# admin.site.register(Match)
+
+
+#example
+# @admin.register(Album)
+# class AlbumAdmin(admin.ModelAdmin):
+#     list_display = ( 'name' , 'release_date' , 'id')
--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    logo = models.ImageField()
+    code = models.CharField(max_length=20)
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    birthdate = models.DateField()
+    age = models.IntegerField()
+    rut = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    height = models.FloatField()
+    weight = models.IntegerField()
+    photo = models.ImageField()
+    position = models.CharField(
+            max_length=2,
+            choices=(
+                    ('BA', 'Base'),
+                    ('ES', 'Escolta'),
+                    ('AL', 'Alero'),
+                    ('AP', 'AlaPivot'),
+                    ('PI', 'Pivot'),
+                    ),
+            default='BA'
+            )
+
+class Couch(models.Model):
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    rut = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+
+class Match(models.Model):
+    name = models.CharField(max_length=100)
